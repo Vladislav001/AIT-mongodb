e752e84caf0f0c7ba99e3a42b848f07d1ccf4943
Vladislav001 <VladGurjev@mail.ru>
2019-01-21 21:57:06 +0400

    Тотальный рефактор 3

--- passport/login.js
+++ passport/login.js
@@ -1,45 +1,39 @@
-var LocalStrategy = require('passport-local').Strategy;
-var User = require('../models/user');
-var bCrypt = require('bcrypt-nodejs');
+const LocalStrategy = require('passport-local').Strategy;
+const User = require('../models/user');
+const bCrypt = require('bcrypt-nodejs');
 
 
-module.exports = function(passport){
+module.exports = function (passport) {
 
   passport.use('login', new LocalStrategy({
-    passReqToCallback : true
+    passReqToCallback: true
   },
-  function(req, email, password, done) {
-    // check in mongo if a user with username exists or not
-    User.findOne({ 'email' : email },
-    function(err, user) {
-      // In case of any error, return using the done method
-      if (err)
-      return done(err);
-      // Username does not exist, log the error and redirect back
-      if (!user){
-        //res.send('Wrong login or password');
-        console.log('User Not Found with username ' + email);
-        return done(null, false, req.flash('message', 'User Not found.'));
-      }
-      // User exists but wrong password, log the error
-      if (!isValidPassword(user, password)){
-      //  res.send('Wrong login or password');
-        console.log('Invalid Password');
-        return done(null, false, req.flash('message', 'Invalid Password')); // redirect back to login page
-      }
-      // User and password both match, return user from done method
-      // which will be treated like success
+    function (req, email, password, done) {
 
-      return done(null, user);
-    }
+      User.findOne({ 'email': email },
+        function (err, user) {
+          if (err)
+            return done(err);
+
+          if (!user) {
+            console.log('User Not Found with username ' + email);
+            return done(null, false, req.flash('message', 'User Not found.'));
+          }
+
+          if (!isValidPassword(user, password)) {
+            console.log('Invalid Password');
+            return done(null, false, req.flash('message', 'Invalid Password')); // redirect back to login page
+          }
+
+          return done(null, user);
+        }
+      );
+    })
   );
 
-})
-);
 
-
-var isValidPassword = function(user, password){
-  return bCrypt.compareSync(password, user.password);
-}
+  const isValidPassword = function (user, password) {
+    return bCrypt.compareSync(password, user.password);
+  }
 
 }
--- passport/signup.js
+++ passport/signup.js
@@ -1,66 +1,63 @@
-var LocalStrategy = require('passport-local').Strategy;
-var User = require('../models/user');
-var bCrypt = require('bcrypt-nodejs');
-var sendMail = require('../functions/sendMail');
+const LocalStrategy = require('passport-local').Strategy;
+const User = require('../models/user');
+const bCrypt = require('bcrypt-nodejs');
+const sendMail = require('../functions/sendMail');
 
-module.exports = function(passport){
+module.exports = function (passport) {
 
   passport.use('signup', new LocalStrategy({
-    passReqToCallback : true // allows us to pass back the entire request to the callback
+    passReqToCallback: true // allows us to pass back the entire request to the callback
   },
-  function(req, email, password, done) {
+    function (req, email, password, done) {
 
-    findOrCreateUser = function(){
-      // find a user in Mongo with provided email
-      User.findOne({ 'email' : email }, function(err, user) {
-        // In case of any error, return using the done method
-        if (err){
-          console.log('Error in SignUp: '+err);
-          return done(err);
-        }
-        // already exists
-        if (user) {
-          console.log('User already exists with email: '+email);
-          return done(null, false, req.flash('message','User Already Exists'));
-        } else {
-          // if there is no user with that email
-          // create the user
-          var newUser = new User();
+      findOrCreateUser = function () {
 
-          // set the user's local credentials
-          newUser.email = email;
-          newUser.password = createHash(password);
+        User.findOne({ 'email': email }, function (err, user) {
+          if (err) {
+            console.log('Error in SignUp: ' + err);
+            return done(err);
+          }
 
-          var currentDate = new Date();
-          newUser.created = currentDate.toUTCString();
+          if (user) {
+            console.log('User already exists with email: ' + email);
+            return done(null, false, req.flash('message', 'User Already Exists'));
+          } else {
 
-          // save the user
-          newUser.save(function(err) {
-            if (err){
-              console.log('Error in Saving user: '+err);
-              throw err;
-            }
-            console.log('User Registration succesful');
+            let newUser = new User();
 
-            // Отправим уведомление на почту
-            sendMail.sendEmailSuccesRegistration(req.headers.host, email);
+            newUser.email = email;
+            newUser.password = createHash(password);
 
-            return done(null, newUser);
-          });
-        }
-      });
-    };
-    // Delay the execution of findOrCreateUser and execute the method
-    // in the next tick of the event loop
-    process.nextTick(findOrCreateUser);
+            let currentDate = new Date();
+            newUser.created = currentDate.toUTCString();
 
-  })
-);
+            newUser.save(function (err) {
+              if (err) {
+                console.log('Error in Saving user: ' + err);
+                throw err;
+              }
+              console.log('User Registration succesful');
 
-// Generates hash using bCrypt
-var createHash = function(password){
-  return bCrypt.hashSync(password, bCrypt.genSaltSync(10), null);
-}
+              // Отправим уведомление на почту
+              sendMail.sendEmailSuccesRegistration(req.headers.host, email);
+
+              return done(null, newUser);
+            });
+          }
+        });
+      };
+
+      // Delay the execution of findOrCreateUser and execute the method
+      // in the next tick of the event loop
+      process.nextTick(findOrCreateUser);
+
+    })
+  );
+
+
+  const createHash = function (password) {
+    return bCrypt.hashSync(password, bCrypt.genSaltSync(10), null);
+  }
 
 
 }
