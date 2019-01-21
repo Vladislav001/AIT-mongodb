790f9d6a2371a3c8090f785b41d0145b89b0998f
Vladislav001 <VladGurjev@mail.ru>
2019-01-21 22:38:56 +0400

    Удалил папку с модулями

--- config.js
+++ config.js
@@ -8,31 +8,3 @@ module.exports = {
       api_secret: 'uXvKOTmYiJ_tC79UFJEMhsVZvvE'
     }
 }
-
-
-
-
-
-// module.exports = {
-//   'url' : 'mongodb://admin:123456@ds155218.mlab.com:55218/cameras'
-// }
-
-// module.exports = {
-//   'url' : 'mongodb://admin:123456@ds155218.mlab.com:55218/cameras'
-// }
-
- // Для локалки
-// 	module.exports = {
-//   'url' : 'mongodb://localhost/passport'
-// }
-
-
-// рабочее
-
-// module.exports = {
-//   'url' : 'mongodb://admin:123456@ds217560.mlab.com:17560/startup_2018'
-// }
-
-// module.exports = {
-//   'url' : 'mongodb://admin:123456@ds217560.mlab.com:17560/startup_2018'
-// }
