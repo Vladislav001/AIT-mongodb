3dca146086cf647ed438bc3b08a68ea046a0bdec
Vladislav001 <VladGurjev@mail.ru>
2019-01-26 15:51:28 +0400

    Рефактор логики для мани гейм

--- routes/applications/money_game/collection_money.js
+++ routes/applications/money_game/collection_money.js
@@ -2,11 +2,11 @@ const PID = require("../../../models/pid");
 const MoneyGame = require("../../../models/money_game");
 const countFiles = require('../../../functions/getCountFilesInDirectory');
 
-exports.get = async function (req, res) { 
+exports.get = async function (req, res) {
 
   try {
 
-    let moneyGame = await MoneyGame.findOne({ pid_id: req.params.idTag }, {'settings': 1, _id: 0});
+    let moneyGame = await MoneyGame.findOne({ pid_id: req.params.idTag }, { 'settings': 1, _id: 0 });
     let pid = await PID.findOne({ _id: req.params.idTag });
 
     res.render("./applications/moneygame/collectionMoney", {
@@ -24,27 +24,26 @@ exports.get = async function (req, res) {
 
 
 exports.post = async function (req, res) {
-  
-  Application.find({ name: 'MoneyGame' }, function (err, application) {
-    var settings;
-    var indexInArray;
 
-    settings = application[0].settings;
-    // Выбирает ВСЕХ студентов - очень плохо, думать как переделать (знать индекс бы сразу)
-    settings.map((item, index) => {
-      for (var key in item) {
-        if (key == req.params.idTag) {
-          indexInArray = index;
-          break;
-        }
+  try {
+
+    let objectSettings = {
+      backBtn: req.body.backBtn,
+      progressBar: req.body.progressBar,
+      nextBtn: req.body.nextBtn, 
+      againBtn: req.body.againBtn,
+      wallet: req.body.wallet,
+      basket: req.body.basket
+    }
+
+    await MoneyGame.updateOne({ pid_id: req.params.idTag }, {
+      $set: {
+        settings: objectSettings
       }
-    })
+    });
 
-    settings[indexInArray][req.params.idTag] = req.body;
-    // if (req.body.progressBar == 'true')
-    settings[indexInArray][req.params.idTag]['progressBar'] = JSON.parse(req.body.progressBar);
-    // else settings[indexInArray][req.params.idTag]['progressBar'] = false;
+  } catch (err) {
+    throw err;
+  }
 
-    Application.update({ name: 'MoneyGame' }, { $set: { settings: settings } }, function (err, data) { });
-  });
 }
