29f7972776566dc3eea306868b860377dfa29a05
Vladislav001 <VladGurjev@mail.ru>
2019-02-24 20:54:59 +0400

    Возвращение всех изображений валют

--- routes/applications/money_game/collection_money.js
+++ routes/applications/money_game/collection_money.js
@@ -4,8 +4,6 @@ const countFiles = require('../../../functions/getCountFilesInDirectory');
 const fs = require('fs');
 
 
-
-
 exports.get = async function (req, res) {
 
   try {
@@ -13,19 +11,29 @@ exports.get = async function (req, res) {
     let moneyGame = await MoneyGame.findOne({ pid_id: req.params._id }, { 'settings': 1, 'currency': 1, _id: 0 });
     let pid = await PID.findOne({ _id: req.params._id });
 
-    let currencyPath = `./public/system_images/currency/${moneyGame.currency}/`;
-    let currencyImages = [];
-    
-    // возможно переделать на async
-    fs.readdirSync(currencyPath).forEach(file => {
-      currencyImages.push(`./currency/${file}`);
+    let availableCurrencies = `./public/system_images/currency/`;
+    let availableCurrenciesDirs = [];
+    let allCurrencies = [];
+
+    // получим название всех папок с валютами
+    fs.readdirSync(availableCurrencies).forEach(dir => {
+      availableCurrenciesDirs.push(dir);
+    });
+
+    // сформируем массив с массивами изображений валют
+    availableCurrenciesDirs.forEach(currencyDir => {
+      let currencyPath = `./public/system_images/currency/${currencyDir}`;
+      allCurrencies[`${currencyDir}`] = [];
+      fs.readdirSync(currencyPath).forEach(currencyImage => {
+        allCurrencies[`${currencyDir}`].push(`/system_images/currency/${currencyDir}/${currencyImage}`);
+      });
     });
 
     res.render("./applications/moneygame/collectionMoney", {
       student: pid,
       settings: JSON.stringify(moneyGame.settings),
       currency: moneyGame.currency,
-      currencyImages: currencyImages,
+      allCurrencies: allCurrencies,
       host: req.headers.host,
       countFiles: countFiles.getCountFilesInDirectoryMoneyGame()
     });
@@ -42,7 +50,7 @@ exports.post = async function (req, res) {
     let objectSettings = {
       backBtn: req.body.backBtn,
       progressBar: req.body.progressBar,
-      nextBtn: req.body.nextBtn, 
+      nextBtn: req.body.nextBtn,
       againBtn: req.body.againBtn,
       wallet: req.body.wallet,
       basket: req.body.basket
@@ -50,9 +58,9 @@ exports.post = async function (req, res) {
 
     let updateData = {
       settings: objectSettings,
-      currency:  req.body.currency ? req.body.currency : "euro"
+      currency: req.body.currency ? req.body.currency : "euro"
     }
-    
+
     await MoneyGame.updateOne({ pid_id: req.params._id }, {
       $set: updateData
     });
