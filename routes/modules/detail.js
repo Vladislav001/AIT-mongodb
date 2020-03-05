533d9e213179ce84c9e5d3a59237c5c4be2cdac5
VladG <v.guriev@lince.studio>
2020-03-05 15:46:29 +0400

    Наработки по выводу readme

--- routes/modules/detail.js
+++ routes/modules/detail.js
@@ -1,15 +1,37 @@
 const Module = require('../../models/module');
 const markdown = require( "markdown" ).markdown;
+var request = require("request"),
+    cheerio = require("cheerio"),
+    url = "https://github.com/steam0111/HallScheme/blob/master/README.md";
 
 exports.get = async function (req, res) {
 
     try {
-        let module = await Module.findOne({_id: req.params._id});
 
-        res.render("modules/detail", {
-            module: module,
-            data: markdown.toHTML( "Hello *World*!" )
+        request(url, async function (error, response, body) {
+            if (!error) {
+                let $ = cheerio.load(body);
+                let readmeData = $("#readme").html();
+                let module = await Module.findOne({_id: req.params._id});
+                readmeData = readmeData.replace(/\n/g, "");
+                console.log(readmeData)
+
+                res.render("modules/detail", {
+                    module: module,
+                    data: markdown.toHTML( "Hello *World*!" ),
+                    readmeData:  readmeData,
+                });
+            } else {
+                console.log("Произошла ошибка: " + error);
+            }
         });
+
+
+
+
+
+
+
     } catch (err) {
         throw err;
     }
