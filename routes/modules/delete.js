70977bb9107cf71a9012497ddc8788265a8fedaa
Vladislav001 <VladGurjev@mail.ru>
2020-05-28 13:50:53 +0400

    Правки по удалению модуля

--- routes/modules/delete.js
+++ routes/modules/delete.js
@@ -3,7 +3,6 @@ const Module = require('../../models/module');
 exports.post = async function (req, res) {
     try {
         await Module.deleteOne({ _id: req.params._id});
-        res.redirect('/modules/1');
     } catch (err) {
         throw err;
     }
