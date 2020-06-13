a7996161c84475594fbaaf596ba757840da20580
Nikita <nikitadmitriev97@mail.ru>
2020-06-13 12:38:48 +0400

    Module integration

--- app.js
+++ app.js
@@ -63,6 +63,12 @@ app.use('/', routes)
 //   next(err);
 // });
 
+app.use(express.static(path.join(__dirname, 'configuration_module')))
+
+app.get('/configuration_module/*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'configuration_module', 'index.html'))
+})
+
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
   next(createError(404))
